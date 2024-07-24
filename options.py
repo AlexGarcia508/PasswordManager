7e146fef45efc0f6ab94e94c2ddcf3fddc8e9f85
Alex Garcia <alex.garcia5082@gmail.com>
2024-07-23 21:52:09 -0700

    alpha finish

--- options.py
+++ options.py
@@ -1,5 +1,91 @@
 from cryptography.fernet import Fernet
 def menu():
+    password = {
+        "discord": "blue",
+        "google": "red"
+    }
+
+    pm = PasswordManager()
+
+    done = False
+
+    while not done:
+        choice = input("Enter choice: ")
+        if choice == "1":
+            path = input("Enter path: ")
+            pm.create_key(path)
+        elif choice == "2":
+            path = input("Enter path: ")
+            pm.load_key(path)
+        elif choice == "3":
+            path = input("Enter path: ")
+            pm.create_password_file(path, password)
+        elif choice == "4":
+            path = input("Enter path: ")
+            pm.load_password_file(path)
+        elif choice == "5":
+            site = input("Enter site: ")
+            password = input("Enter password: ")
+            pm.add_password(site, password)
+        elif choice == "6":
+            site = input("What site do you want?: ")
+            print(f"Password for {site} is {pm.get_password(site)}")
+        elif choice == "7":
+            done = True
+            print("Bye")
+        else:
+            print("Invalid choice.")
+
+class PasswordManager:
+    def __init__(self):
+        self.key = None
+        self.password_file = None
+        self.password_dict = {}
+
+    def create_key(self, path):
+        self.key = Fernet.generate_key()
+        print(self.key)
+        with open(path, 'wb') as f:
+            f.write(self.key)
+
+    def load_key(self, path):
+        with open(path, 'rb') as f:
+            self.key = f.read()
+
+    def create_password_file(self, path, initial_values=None):
+        self.password_file = path
+
+        if initial_values is not None:
+            for key, value in initial_values.items():
+                self.add_password(key, value)
+
+    def load_password_file(self, path):
+        self.password_file = path
+
+        with open(path, 'r') as f:
+            for line in f:
+                site, encrypted = line.split(":")
+                self.password_dict[site] = Fernet(self.key).decrypt(encrypted.encode()).decode()
+
+    def add_password(self, site, password):
+        self.password_dict[site] = password
+
+        if self.password_file is not None:
+            with open(self.password_file, 'a') as f:
+                encrypted = Fernet(self.key).encrypt(password.encode())
+                f.write(site + ":" + encrypted.decode() + "\n")
+
+    def get_password(self, site):
+        return self.password_dict[site]
+
+    def delete_password(self):
+        pass
+
+    def change_password(self):
+        pass
+
+'''
+def menu():
     print("Welcome to Password Manager.")
     print("1. Access passwords.")
     print("2. Add new password.")
@@ -19,34 +105,4 @@ def menu():
         delete_password()
     elif selection == 4:
         change_password()
-
-class PasswordManager:
-    def __init__(self):
-        self.key = None
-        self.password_file = None
-        self.password_dict = {}
-
-    def create_key(self, path):
-        self.key = Fernet.generate_key()
-        print(self.key)
-        with open(path, 'wb') as f:
-            f.write(self.key)
-
-    def load_key(self, path):
-        with open(path, 'rb') as f:
-            self.key = f.read()
-
-def password_log():
-    pass
-
-
-def add_password():
-    pass
-
-
-def delete_password():
-    pass
-
-
-def change_password():
-    pass
+'''
